df7dd3737513e8d72fdcb82b8af3701078022cbe
Harish Balaji <hariahbalaji672@gmail.com>
2026-02-22 13:54:37 +0530

    Create 2058-ConcatenationOfArray

---
+++ 2058-ConcatenationOfArray/2058-ConcatenationOfArray.c
@@ -0,0 +1,13 @@
+// Last updated: 2/22/2026, 1:54:31 PM
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+    *returnSize = 2*numsSize;
+    int* result = (int*)malloc(2*numsSize*sizeof(int));
+    for(int i = 0 ; i < 2*numsSize ; i++)
+    {
+        result[i] = nums[i%numsSize];
+    }
+    return result;
+}
