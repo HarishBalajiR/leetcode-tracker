08f5160d1a752cc01dc9f28e44e9187b86b65e9d
Harish Balaji <hariahbalaji672@gmail.com>
2026-02-27 18:06:42 +0530

    Create 191-Numberof1Bits

---
+++ 191-Numberof1Bits/version/java/191-Numberof1Bits_20260227_180626.java
@@ -0,0 +1,7 @@
+// Last updated: 2/27/2026, 6:06:26 PM
+// Integer.bitCount -> Returns set bits of an integer.
+1class Solution {
+2    public int hammingWeight(int n) {
+3        return Integer.bitCount(n);
+4    }
+5}
