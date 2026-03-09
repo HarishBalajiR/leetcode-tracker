ffd91b93f7b6dbf39da6b5721d679896ff668b14
Harish Balaji <hariahbalaji672@gmail.com>
2026-03-09 11:34:52 +0530

    Create 2769-FindtheMaximumAchievableNumber

---
+++ 2769-FindtheMaximumAchievableNumber/version/cpp/2769-FindtheMaximumAchievableNumber_20260309_113416.cpp
@@ -0,0 +1,13 @@
+// Last updated: 3/9/2026, 11:34:16 AM
+/*
+ * Same approach as Java.
+ * Refer Java Notes.
+ * achievable description is explained there.
+*/
+
+1class Solution {
+2public:
+3    int theMaximumAchievableX(int num, int t) {
+4        return num+2*t;
+5    }
+6};
