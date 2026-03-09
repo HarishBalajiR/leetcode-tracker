c0edaaf38db0818f181a5cf2d05f4a3c3e4c1f6e
Harish Balaji <hariahbalaji672@gmail.com>
2026-03-09 12:04:44 +0530

    Create 2769-FindtheMaximumAchievableNumber

---
+++ 2769-FindtheMaximumAchievableNumber/version/java/2769-FindtheMaximumAchievableNumber_20260309_120408.java
@@ -0,0 +1,13 @@
+// Last updated: 3/9/2026, 12:04:08 PM
+/*
+ * No number cannot be strictly palindromic with the given constraints.
+ * Therefore false is returned.
+ * Any number 'n' return "12" in base n-2.
+ * 12 is not a palindrome so false always.
+*/
+
+1class Solution {
+2    public boolean isStrictlyPalindromic(int n) {
+3        return false;
+4    }
+5}
