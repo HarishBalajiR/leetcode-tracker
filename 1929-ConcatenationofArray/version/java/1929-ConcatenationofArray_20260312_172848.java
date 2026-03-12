f11e10a9b06d973e5a997d26c0f85249effd3c94
Harish Balaji <hariahbalaji672@gmail.com>
2026-03-12 17:29:30 +0530

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/version/java/1929-ConcatenationofArray_20260312_172848.java
@@ -0,0 +1,10 @@
+// Last updated: 3/12/2026, 5:28:48 PM
+1class Solution {
+2    public int[] buildArray(int[] nums) {
+3        int[] arr = new int[nums.length];
+4        for(int i = 0 ; i < nums.length ; i++){
+5            arr[i] = nums[nums[i]];
+6        }
+7        return arr;
+8    }
+9}
