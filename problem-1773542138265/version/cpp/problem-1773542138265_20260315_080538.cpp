4c4db5e42384b09c5f08079b82b02d301a16c8eb
Harish Balaji <hariahbalaji672@gmail.com>
2026-03-15 08:06:24 +0530

    Create

---
+++ problem-1773542138265/version/cpp/problem-1773542138265_20260315_080538.cpp
@@ -0,0 +1,13 @@
+// Last updated: 3/15/2026, 8:05:38 AM
+// Nice.
+1class Solution {
+2public:
+3    int countCommas(int num) {
+4        string s = to_string(num);
+5        int n = s.size();
+6        if(n<=3) return 0;
+7        else{
+8            return num - 999;
+9        }
+10    }
+11};
